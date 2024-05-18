122c29fa5197fc4439ea5e81b9e1d013cb84b29f
Victor <vvalord0211@iesfuengirola.es>
2024-05-18 13:09:24 +0200

    created components and style for the menu

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,7 +1,15 @@
 <template>
-    <h1>header</h1>
+    <header class="menu">
+        <Menu/>
+    </header>
+    <section>
+        <div>
+            <slot />
+        </div>
+    </section>
 </template>
 
 <script setup>
+import Menu from './Menu.vue';
 
 </script>
---
+++ resources/js/Shared/MenuLink.vue
@@ -0,0 +1,11 @@
+<template>
+    <Link href="/" class="menu__list__item__link" :class="{'menu__list__item--active':active}">
+        <slot/>
+    </Link>
+</template>
+
+<script setup>
+defineProps({
+    active: Boolean
+})
+</script>
