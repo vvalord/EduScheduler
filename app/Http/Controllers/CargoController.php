7b0d5db9f8cf25b5aca0445a77ed6297964f6a64
Juan Jesús <101215545+juanje00000@users.noreply.github.com>
2024-05-23 16:39:51 +0200

    CargoC. api beta

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 use App\Models\Cargo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CargoController extends Controller
 {
     //Si ves esto, ignora este controller
-    public function insert()
+    public static function insert()
     {
         $datos = request()->validate([
             'horas' => ['required', 'min:0','max:18', 'integer']
@@ -27,13 +29,14 @@ class CargoController extends Controller
     }
 
     public function searchAll(){
-        //Hecho asi y no como consulta preparada por que supuestamente en limit y offset no funcionan las variables preparadas
-        //Probar como preparada antes de dejarla como definitiva ':offset'
-        $cargos=DB::select("SELECT * FROM cargos;");
+        $cargos=Cargo::all()->through(fn($cargo)=>[
+            'id'=>$cargo->id,
+            'nombre'=>$cargo->nombre
+        ]);
         return $cargos;
     }
-    public function search(?int $page,?string $name){
-        if($page==null){
+    public static function search(){
+        /*if($page==null){
             $page=1;
         }
         $offset=10*($page-1);
@@ -43,40 +46,60 @@ class CargoController extends Controller
         //Hecho asi y no como consulta preparada por que supuestamente en limit y offset no funcionan las variables preparadas
         //Probar como preparada antes de dejarla como definitiva ':offset'
         $cargos=DB::select("SELECT * FROM cargos where nombre like '%?%' limit 10 OFFSET $offset;",[1, $name]);
+        return $cargos;*/
+        $cargos=Cargo::query()
+        ->when(Request::input('search'),function ($query,$search){
+            $query->where('nombre','like',"%{$search}%");
+        })
+        ->paginate(10)
+        ->withQueryString()
+        ->through(fn($cargo)=>[
+            'id'=>$cargo->id,
+            'nombre'=>$cargo->nombre
+        ]);
         return $cargos;
     }
-    public function searchById(int $id){
-        $cargo=DB::select("SELECT * FROM cargos where id=?",[1, $id]);
+    public static function searchById(int $id){
+        //$cargo=DB::select("SELECT * FROM cargos where id=?",[1, $id]);
+        $cargo=Cargo::query()->find($id);
         return $cargo;
     }
 
-    public function update(int $id){
+    public static function update(int $id){
         $datos = request()->validate([
             'horas' => ['required', 'min:0','max:18', 'integer']
         ]);
         $cargo=CargoController::searchById($id);
-        if($datos['nombre']!=$cargo['nombre']){
-            DB::update(
+        if($datos['nombre']!=$cargo->nombre||$datos['horario']!=$cargo->horario||$datos['horas']!=$cargo->horas){
+        if($datos['nombre']!=$cargo->nombre){
+            $cargo->nombre=$datos['nombre'];
+            /*DB::update(
                 'update cargos set nombre = ? where id = ?',
                 [$datos['nombre'],$id]
-            );
+            );*/
         }
-        if($datos['horas']!=$cargo['horas']){
-            DB::update(
+        if($datos['horas']!=$cargo->horas){
+            $cargo->horas=$datos['horas'];
+            /*DB::update(
                 'update cargos set horas = ? where id = ?',
                 [$datos['horas'],$id]
-            );
+            );*/
         }
-        if($datos['horario']!=$cargo['horario']){
-            DB::update(
+        if($datos['horario']!=$cargo->horario){
+            $cargo->horario=$datos['horario'];
+            /*DB::update(
                 'update cargos set horario = ? where id = ?',
                 [$datos['horario'],$id]
-            );
+            );*/
         }
-        return redirect('/cargos');
+        $cargo->save();
     }
-    public function delete(int $id){
-        $deleted = DB::delete('delete from cargos where id=?',[1, $id]);
+        return redirect('/cargos');
+
+    }
+    public static function delete(int $id){
+        //$deleted = DB::delete('delete from cargos where id=?',[1, $id]);
+        Cargo::query()->where('id',$id)->delete();
         return redirect('/cargos');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CargoController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -19,6 +21,33 @@ Route::get('/cursos', function () {
     return Inertia::render('Cursos');
 });
 
+//Listado cargos
 Route::get('/cargos', function () {
-    return Inertia::render('Cargos');
+    return Inertia::render('Cargos',[
+        'cargos'=>CargoController::search(),
+        //Para el input de busqueda
+        'filters'=>Request::only(['search'])
+    ]);
+});
+//Enviar datos from Cargo
+Route::post('/cargos/create',function(){
+    CargoController::insert();
+});
+
+//Update form
+Route::get('/cargo/{id}',function(){
+    return Inertia::render('CargosForm',[
+        //Obtener los dato del cargo para introudcirlo en el input
+        'cargo'=>CargoController::searchById($id)
+    ]);
+});
+
+//Update form sent data
+Route::put('/cargo/{id}',function(){
+    CargoController::update($id);
+});
+
+//
+Route::delete('/cargo/{id}',function(){
+    CargoController::delete($id);
 });
